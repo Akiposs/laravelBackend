2f8660f2a5d0202212d92b38f928556ad8a3e511
Akihiro_Takano <superakip1995@gmail.com>
2020-03-17 00:27:21 +0900

    一時保存

--- mysql/initdb.d/init.sql
+++ mysql/initdb.d/init.sql
@@ -1,7 +1,5 @@
-CREATE DATABASE schedule_manage_db;
-CREATE USER 'schedule_mng_user'@'phpfpm' IDENTIFIED BY 'shcedule_mtg_pass';
-GRANT ALL ON shcedule_manage_db.* TO 'schedule_mng_user'@'phpfpm';
+-- CREATE DATABASE schedule_manage_db;
+-- CREATE USER 'schedule_mng_user'@'%' IDENTIFIED BY 'shcedule_mtg_pass';
+-- GRANT ALL ON *.* TO 'schedule_mng_user'@'%';
 
 
--- CREATE USER 'test'@'phpfpm' IDENTIFIED BY 'test';
--- GRANT ALL ON *.* TO 'schedule_mng_user4'@'172.30.0.3';
